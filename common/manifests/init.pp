22c490c8c5a63ed469ef6f84c57c90192c4b5a45
root <root@puppet.example42.com>
2010-01-08 05:04:59 -0500

    Alkigned init.pp to DavidS' set

--- common/manifests/init.pp
+++ common/manifests/init.pp
@@ -1,2 +1,16 @@
+
+file {
+        # Module programmers can use /var/lib/puppet/modules/$modulename to save
+        # module-local data, e.g. for constructing config files
+        "/var/lib/puppet/modules":
+                ensure => directory,
+#                source => "puppet://$server/modules/common/modules/",
+#                ignore => '\.ignore',
+#                recurse => true, purge => true, force => true,
+                mode => 0755, owner => root, group => 0;
+}
+
+
+
 import "classes/*"
 import "defines/*"
