f849c11ddc9e4f2da51ec162274675d8a5cd8f0e
root <root@puppet.example42.com>
2010-01-28 12:23:50 +0100

    Fixed: err: Could not retrieve catalog: Could not parse for environment production: Syntax error at '/'; expected '}' at /etc/puppet/modules/iptables/manifests/init.pp:4
    Syntax compatible with Puppet 0.25.x but not with 0.24.8

--- iptables/manifests/init.pp
+++ iptables/manifests/init.pp
@@ -1,7 +1,8 @@
 class iptables {
 
 	case $operatingsystem {
-        	/centos|redhat/: { include iptables::redhat }
+        	centos: { include iptables::redhat }
+        	redhat: { include iptables::redhat }
         	default: { warning("No such operatingsystem: $operatingsystem yet defined") }
     	}
 
