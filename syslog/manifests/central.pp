7d470bdbcf34a25e9497b4d8bf3a38e2042e67d2
root <root@puppet.example42.com>
2010-02-09 17:05:09 +0100

    Added comment on modified lines

--- syslog/manifests/central.pp
+++ syslog/manifests/central.pp
@@ -2,7 +2,7 @@ class syslog::central inherits syslog::base {
 
         config { "syslog.conf" :
                 file      => "/etc/syslog.conf",
-                line      => "*.*	@$syslog_server",
+                line      => "*.*	@$syslog_server # Added by Puppet",
                 engine    => "line",
 		notify    => Service["syslog"],
         }
