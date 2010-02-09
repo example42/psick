79fc38f331cddf34fd775ec93724bd884e145896
root <root@puppet.example42.com>
2010-02-09 17:04:17 +0100

    Debian/Redhat support

--- nrpe/manifests/init.pp
+++ nrpe/manifests/init.pp
@@ -1,79 +1,27 @@
 class nrpe {
-        package {
+
+	package {
                 "nrpe":
                         ensure => "present",
-			name   => $extrarepo ? {
-				epel     => "nrpe",
-				rpmforge => "nagios-nrpe",
-				default  => "nrpe",
-        		};
+			name   => $operatingsystem ? {
+                                ubuntu  => "nagios-nrpe-server",
+                                debian  => "nagios-nrpe-server",
+                                centos  => "nrpe", # OK for EPEL, use nagios-nrpe for RPMFORGE
+                                redhat  => "nrpe", # OK for EPEL, use nagios-nrpe for RPMFORGE
+			},
         }
 	
-	case $extrarepo {
-		epel: { 
-        		package {
-				"nagios-plugins-dig":
-                        		ensure => "present";
-				"nagios-plugins-disk":
-                        		ensure => "present";
-				"nagios-plugins-dns":
-                        		ensure => "present";
-				"nagios-plugins-file_age":
-                        		ensure => "present";
-				"nagios-plugins-http":
-                        		ensure => "present";
-				"nagios-plugins-ldap":
-                        		ensure => "present";
-				"nagios-plugins-load":
-                        		ensure => "present";
-				"nagios-plugins-log":
-                        		ensure => "present";
-				"nagios-plugins-mysql":
-                        		ensure => "present";
-				"nagios-plugins-nagios":
-                        		ensure => "present";
-				"nagios-plugins-nrpe":
-                        		ensure => "present";
-				"nagios-plugins-ntp":
-                        		ensure => "present";
-				"nagios-plugins-ping":
-                        		ensure => "present";
-				"nagios-plugins-procs":
-                        		ensure => "present";
-				"nagios-plugins-rpc":
-                        		ensure => "present";
-				"nagios-plugins-smtp":
-                        		ensure => "present";
-				"nagios-plugins-snmp":
-                        		ensure => "present";
-				"nagios-plugins-ssh":
-                        		ensure => "present";
-				"nagios-plugins-swap":
-                        		ensure => "present";
-				"nagios-plugins-tcp":
-                        		ensure => "present";
-				"nagios-plugins-udp":
-                        		ensure => "present";
-				"nagios-plugins-users":
-                        		ensure => "present";
-        		}
-        	}
-
-		rpmforge: { 
-        		package {
-				"nagios-plugins":
-                        		ensure => "present";
-        		}
-        	}
-
-		default: { 
-        		package {
-				"nagios-plugins":
-                        		ensure => "present";
-        		}
-        	}
+	package {
+                "nagios-plugins":
+                        ensure => "present",
+			name   => $operatingsystem ? {
+                                ubuntu  => "nagios-plugins",
+                                debian  => "nagios-plugins",
+                                centos  => "nagios-plugins-all", # OK for EPEL, use nagios-plugins for RPMFORGE
+                                redhat  => "nagios-plugins-all", # OK for EPEL, use nagios-plugins for RPMFORGE
+			},
         }
-        
+	
 	service {
                 "nrpe":
                         enable    => "true",
@@ -83,13 +31,18 @@ class nrpe {
                         ],
                         ensure    => "running",
                         subscribe => File["nrpe.cfg"],
+			name => $operatingsystem ? {
+                                ubuntu  => "nagios-nrpe-server",
+                                debian  => "nagios-nrpe-server",
+                                centos  => "nrpe",
+                                redhat  => "nrpe",
+                        },
+			pattern   => "nrpe",
         }
 
         file {
                 "nrpe.cfg":
-                        owner   => root,
-                        group   => root,
-                        mode    => 644,
+#                        owner => root, group => root, mode => 644,
                         ensure	=> present,
                         require => Package["nrpe"],
                         path    => $operatingsystem ?{
