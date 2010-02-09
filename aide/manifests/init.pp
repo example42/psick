7b606fc92f138fd59643af2e364db7e3d62376a8
root <root@puppet.example42.com>
2010-02-09 17:00:16 +0100

    Debian/RedHat support for Aide

--- aide/manifests/init.pp
+++ aide/manifests/init.pp
@@ -9,24 +9,28 @@ class aide {
 
 	file {	
              	"aide.conf":
-			mode => 600, owner => root, group => root,
+#			mode => 600, owner => root, group => root,
 			require => Package["aide"],
 			ensure => present,
 			path => $operatingsystem ?{
-                        	default => "/etc/aide.conf",
+                        	debian  => "/etc/aide/aide.conf",
+	                        ubuntu  => "/etc/aide/aide.conf",
+        	               	default => "/etc/aide.conf",
                         },
 	}
 
-	file {	
-             	"aide.sh":
-			mode => 750, owner => root, group => root,
-			require => File["aide.conf"],
-			ensure => present,
-			path => $operatingsystem ?{
-                        	default => "/etc/cron.daily/aide",
-                        },
-			source => "puppet://$server/aide/aide.sh",
-	}
+# Cron job
+#	file {	
+#             	"aide.sh":
+#			mode => 750,
+#			owner => root, group => root,
+#			require => File["aide.conf"],
+#			ensure => present,
+#			path => $operatingsystem ?{
+#                        	default => "/etc/cron.daily/aide",
+#                        },
+#			source => "puppet://$server/aide/aide.sh",
+#	}
 	
 }
 
