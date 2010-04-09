efe2467cceec5a9d6fc7145b7c0052eabdc015ec
root <root@kick.lab42.it>
2010-04-09 18:47:52 +0200

    New mysql module

---
+++ mysql/manifests/classes/backup.pp
@@ -0,0 +1,20 @@
+# Class: mysql::backup
+#
+# Backups mysql data directory and, optionally, logs (must be enabled)
+# It's automatically included if $backup=yes
+#
+# Usage:
+# include mysql::backup
+
+class mysql::backup {
+
+	backup { "mysql_data": 
+		frequency => daily,
+		path	=> $operatingsystem ?{
+                        default => "/var/lib/mysql",
+                },		
+		enabled	=> true,
+		host => $fqdn,
+	}
+	
+}
---
+++ mysql/manifests/classes/monitor.pp
@@ -0,0 +1,19 @@
+# Class: mysql::monitor
+#
+# Monitors mysql process and port using custom Monitor wrapper
+# It's automatically included if $monitor=yes
+#
+# Usage:
+# include mysql::monitor
+
+class mysql::monitor {
+
+	monitor::process {
+		"mysql_process":
+                name 	=> $operatingsystem ? {
+                        default => "mysqld",
+                        },
+		enable	=> true,
+	}
+
+}
