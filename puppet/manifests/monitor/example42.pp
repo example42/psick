0aef86ae5341f465de3be374ef11a7d64e96f61b
root <root@puppet.example42.com>
2010-10-26 23:23:25 +0200

    Added missing monitor/example42 classes

---
+++ puppet/manifests/monitor/example42.pp
@@ -0,0 +1,8 @@
+# Class puppet::monitor::example42
+#
+# Custom monitor class for example42 project.
+# Here you can override or define your settings for the monitor wrapper
+#
+class puppet::monitor::example42 inherits puppet::monitor {
+
+}
