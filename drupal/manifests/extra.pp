0ae47b5596b2d9223122c4422abf8414ae13ec9b
root <root@puppet.example42.com>
2011-01-22 10:23:03 +0100

    Added some extra modules to drupal extra

--- drupal/manifests/extra.pp
+++ drupal/manifests/extra.pp
@@ -9,16 +9,26 @@
 #
 class drupal::extra {
 
+    drupal::module { "ctools":  }
     drupal::module { "views":  }
+    drupal::module { "panels":  }
+    drupal::module { "multiblock":  }
+    drupal::module { "admin_menu":  }
+    drupal::module { "advanced_help":  }
+
     drupal::module { "cck":  }
     drupal::module { "imce":  }
     drupal::module { "xml_sitemap":  }
     drupal::module { "webform":  }
     drupal::module { "google_analytics":  }
+    drupal::module { "seo_checklist":  }
     drupal::module { "github":  }
     drupal::module { "gitbrowser":  }
     drupal::module { "flickr":  }
     drupal::module { "wysiwyg":  }
+    drupal::module { "jwysiwyg":  }
+    drupal::module { "wymeditor":  }
+
     drupal::module { "addtoany":  }
     drupal::module { "syntaxhighlighter":  }
     drupal::theme { "zen":  }
