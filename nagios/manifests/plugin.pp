98f4bd8c654895edb68c6e15428c659cabf0607a
Al <al@Als-MacBook-Pro.local>
2011-01-28 18:51:22 +0100

    Added Nagios extra plugins and base checks

---
+++ nagios/manifests/plugin.pp
@@ -0,0 +1,30 @@
+# Define: nagios::plugin
+#
+# Adds a custom Nagios plugin
+#
+# Usage:
+# With standard template:
+# nagios::plugin { "check_mount": }
+#
+define nagios::plugin ( $enable="yes" ) {
+
+   include nagios::params
+   include nrpe::params
+
+    $ensure = $enable ? {
+        "false" => "absent",
+        "no"    => "absent",
+        "true"  => "present",
+        "yes"   => "present",
+    }
+
+   file { "Nagios_plugin_${name}":
+        path    => "${nrpe::params::pluginsdir}/${name}",
+        owner   => root,
+        group   => root,
+        mode    => 755,
+        ensure  => "${ensure}",
+        source  => "${nagios::params::general_base_source}/nagios/nagios-plugins/${name}",
+   }
+
+}
