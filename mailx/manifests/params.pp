d3f4d10798706dde6b602a35fcb5bf689eef34e9
Al <al@Als-MacBook-Pro.local>
2011-01-28 20:02:42 +0100

    Mailx module

---
+++ mailx/manifests/params.pp
@@ -0,0 +1,17 @@
+# Class: mailx::params
+#
+# Defines mailx parameters
+# In this class are defined as variables values that are used in other mailx classes
+# This class should be included, where necessary, and eventually be enhanced with support for more OS
+#
+class mailx::params  {
+
+## MODULES INTERNAL VARIABLES
+# (Modify only to adapt to unsupported OSes)
+
+    $packagename = $operatingsystem ? {
+        debian  => "bsd-mailx",
+        ubuntu  => "bsd-mailx",
+        default => "mailx",
+    }
+}
