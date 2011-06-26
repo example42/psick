c6077c9a013e21aa1696406b9a8eb240d70d029b
Alessandro Franceschi <al@lab42.it>
2011-06-26 18:19:15 +0200

    Updated massfindandreplace.sh script

--- 00_example42scripts/massfindandreplace.sh
+++ 00_example42scripts/massfindandreplace.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-OLDPROJECT=" "
-NEWPROJECT=" "
+OLDSTRING=$1
+NEWSTRING=$2
 
-for file in $( grep -R "$OLDPROJECT" . | grep -v ".git" | cut -d ":" -f 1 ) ; do
-        sed -i "s/$OLDPROJECT/$NEWPROJECT/g" $file && echo "Changed $file" # Use under Linux
+if [ ! $1 ] ; then
+    echo "Usage: $0 oldstring newstring"
+    exit 1
+fi
+for file in $( grep -R "$OLDSTRING" . | grep -v ".git" | cut -d ":" -f 1 ) ; do
+        sed -i "s/$OLDSTRING/$NEWSTRING/g" $file && echo "Changed $file" # Use under Linux
 done
