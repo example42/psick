e8c0b0c032cabb6cbb972f09adce55b3bc123206
root <root@puppet.example42.com>
2011-10-02 11:08:22 +0200

    Refactored common class, definition of common::osver variable to avoid usage of lsb facts

--- common/manifests/init.pp
+++ common/manifests/init.pp
@@ -9,8 +9,29 @@ file {
         mode => 0755, owner => root, group => 0;
 }
 
+import "*.pp"
 
+class common {
+# Some General Use Variables
 
-# import "classes/*"
-import "defines/*"
+# Calculate OS version (without using lsb facts)
+    $ossplit=split($operatingsystemrelease, '[.]')
+    $osver=$ossplit[0]
 
+## FILE SERVING SOURCE
+# Sets the correct source for static files
+# In order to provide files from different sources without modifyingi a module
+# you can override the default source path setting the variable $base_source
+# Ex: $base_source="puppet://ip.of.fileserver" or $base_source="puppet://$servername/myprojectmodule"
+# What follows automatically manages the source standard for Puppet versions pre and post 0.25
+    case $base_source {
+        '': {
+            $source = $puppetversion ? {
+                /(^0.25)/ => "puppet:///modules",
+                /(^0.)/   => "puppet://$servername",
+                default   => "puppet:///modules",
+            }
+        }
+        default: { $source=$base_source }
+    }
+}
