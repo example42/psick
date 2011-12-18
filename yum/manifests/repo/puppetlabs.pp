c4b88f622986ae7cefb6ecb5339c6d3ac02452e9
Alessandro Franceschi <al@lab42.it>
2011-12-18 17:03:47 +0100

    Updated Puppetlabs yum repo

--- yum/manifests/repo/puppetlabs.pp
+++ yum/manifests/repo/puppetlabs.pp
@@ -2,7 +2,17 @@ class yum::repo::puppetlabs {
 
     yum::managed_yumrepo { puppetlabs:
         descr => 'Puppet Labs Packages',
-        baseurl => 'http://yum.puppetlabs.com/base/',
+        baseurl => 'http://yum.puppetlabs.com/el/$releasever/products/$basearch',
+        enabled => 1,
+        gpgcheck => 1,
+        failovermethod => 'priority',
+        gpgkey => 'http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs',
+        priority => 15,
+    }
+
+    yum::managed_yumrepo { puppetlabs_dependencies:
+        descr => 'Puppet Labs Packages',
+        baseurl => 'http://yum.puppetlabs.com/el/$releasever/dependencies/$basearch',
         enabled => 1,
         gpgcheck => 1,
         failovermethod => 'priority',
