238aa62bef344ad2f047a039d43d3d57b832b169
Alessandro Franceschi <al@lab42.it>
2012-01-12 11:00:40 +0100

    Added spunk and monitor submodules

--- monitor/lib/puppet/parser/functions/urlhostname.rb
+++
@@ -1,9 +0,0 @@
-# require 'uri'
-
-module Puppet::Parser::Functions
-        newfunction(:urlhostname, :type => :rvalue, :doc => "Extracts the hostname from an url" ) do |args|
-                args[0].split('/')[2]
-                # URI.parse(args[0]).host
-        end
-end
-
--- monitor/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-# Experimental monitor abstraction module.
-# This can be used to monitor hosts and applications with different methods.
-#
-#
-
-class monitor {
-
-}
-
