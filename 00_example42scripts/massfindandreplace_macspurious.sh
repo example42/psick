2360aac874f29c20704c4b45b9a634acb1d7e8b2
Alessandro Franceschi <al@lab42.it>
2012-01-21 14:23:26 +0100

    00_example42scripts substituted with Example42-tools submodule

--- 00_example42scripts/massfindandreplace_macspurious.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-OLDPROJECT=" "
-NEWPROJECT=" "
-
-for file in $( grep -R "$OLDPROJECT" . | grep -v ".git" | grep -v $0 | cut -d ":" -f 1 ) ; do
-        sed -i "s/$OLDPROJECT/$NEWPROJECT/g" $file && echo "Changed $file" # Use under Linux
-done
