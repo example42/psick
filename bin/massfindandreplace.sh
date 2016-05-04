49c5968e536f6a2cdb1c58298cd3ac58b8cdd356
Alessandro Franceschi <al@example42.com>
2016-05-04 14:49:15 +0200

    Updated to use docker and apache 4.x modules
    * Using docker profile from (new) docker module
    
    * Updated Puppetfile for apache 4.x

--- bin/massfindandreplace.sh
+++ bin/massfindandreplace.sh
@@ -7,5 +7,10 @@ if [ ! $1 ] ; then
     exit 1
 fi
 for file in $( grep -R "$OLDSTRING" . | grep -v ".git" | cut -d ":" -f 1 ) ; do
-        sed -i "s/$OLDSTRING/$NEWSTRING/g" $file && echo "Changed $file" # Use under Linux
+    # Detect OS
+    if [ -f /System/Library/Accessibility/AccessibilityDefinitions.plist ] ; then
+      sed -i "" -e "s/$OLDSTRING/$NEWSTRING/g" $file && echo "Changed $file"
+    else
+      sed -i "s/$OLDSTRING/$NEWSTRING/g" $file && echo "Changed $file"
+    fi
 done
