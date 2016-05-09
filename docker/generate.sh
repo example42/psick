11b392ce8b3c632b5b9cb3bffce7486e4cfeee47
Alessandro Franceschi <al@example42.com>
2016-05-09 16:11:57 +0200

    Initial docker setup (#64)
    * Updated entry points for tpweb
    
    * Imported stuff from tp_dockerfiles
    
    * Initial Docker setup

---
+++ docker/generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+  echo "Type the OS you want to use for base image"
+  ls -1 env/* | sed 's/env\///g'
+  read os
+  echo
+else
+  os=$1
+fi
+
+. config
+. env/${os}
+echo
+echo "# Generating Dockerfile for ${FACTER_operatingsystem} ${FACTER_operatingsystemmajrelease}"
+puppet apply -t --basemodulepath "../modules" --hiera_config "hiera.yaml" -e 'include ::docker::profile::builder' 
