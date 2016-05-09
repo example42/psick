2672baa1120e88c0d2373c70fedea4b5ea4b4af8
Alessandro Franceschi <al@example42.com>
2016-05-09 22:02:38 +0200

    Finalising control-repo testing on docker containers

--- bin/papply_docker.sh
+++ bin/papply_docker.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
+image=$1
 PATH=$PATH:/opt/puppetlabs/bin
 
-echo "## Running Puppet version $(puppet --version) on ${manifest}"
-
-docker run -v .:/docker $1 puppet apply --verbose --report --show_diff --pluginsync --summarize --modulepath "/docker/site:/docker/modules:/etc/puppet/modules" --hiera_config=/docker/hiera.yaml --detailed-exitcodes $manifest_option $manifest
+docker run -v /Users/al/Documents/github/EXAMPLE42/puppet-modules:/etc/puppetlabs/code/environments/production $1 /etc/puppetlabs/code/environments/production/bin/papply.sh --detailed-exitcodes 
 
 if [ "x$?" == "x0" ] || [ "x$?" == "x1" ]; then
   exit 0
