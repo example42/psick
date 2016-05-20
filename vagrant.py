4d79a8e52cb71afe77bed34ec8c778a9e34caa55
Alessandro Franceschi <al@example42.com>
2016-05-20 18:13:27 +0200

    More Vagrant tasks for Fabric

--- vagrant.py
+++ vagrant.py
@@ -1,22 +1,47 @@
 from fabric.api import *
 
 @task
-def status(env=''):
+def all_status(env=''):
   """Run vagrant status on all the available environments"""
-  local( 'cd vagrant/environments ; for v in $(ls ' + str(env) + '); do cd $v ; echo $v ; vagrant status ; cd ../ ; echo ; done' )
+  local( 'cd vagrant/environments ; for v in $(ls ' + str(env) + '); do cd $v ; echo "Vagrant environment: ${v}" ; echo ; vagrant status ; cd ../ ; echo ; done' )
 
 @task
-def provision(env='',vm=''):
-  """Run vagrant provision on all the VMs or the specified one"""
-  local( 'cd vagrant/environments ; for v in $(ls ' + str(env) + '); do cd $v ; echo $v ; vagrant provision ' + str(vm) + '; cd ../ ; echo ; done' )
+def status(vm=''):
+  """Show status of the specified vm"""
+  local( 'vagrant/bin/vm.sh status ' + str(vm) )
 
 @task
-def up(env='',vm=''):
-  """Run vagrant up on all the VMs (CAUTION!) or the specified one"""
-  local( 'cd vagrant/environments ; for v in $(ls ' + str(env) + '); do cd $v ; echo $v ; vagrant up ' + str(vm) + '; cd ../ ; echo ; done' )
+def suspend(vm):
+  """Suspend the specified vm"""
+  local( 'vagrant/bin/vm.sh suspend ' + str(vm) )
 
 @task
-def ssh(vm):
-  """Run vagrant ssh on the specified vm"""
-  local( 'vagrant/bin/vm.sh ssh ' + str(vm) )
+def resume(vm):
+  """Resume the specified vm"""
+  local( 'vagrant/bin/vm.sh resume ' + str(vm) )
+
+@task
+def destroy(vm):
+  """Destroy the specified vm"""
+  local( 'vagrant/bin/vm.sh destroy ' + str(vm) )
+
+@task
+def reload(vm):
+  """Reload the specified vm"""
+  local( 'vagrant/bin/vm.sh reload ' + str(vm) )
+
+@task
+def provision(vm):
+  """Run vagrant provision on the specified vm"""
+  local( 'vagrant/bin/vm.sh provision ' + str(vm) )
+
+@task
+def up(vm):
+  """Vagrant up the specified vm"""
+  local( 'vagrant/bin/vm.sh up ' + str(vm) )
+
+@task
+def halt(vm):
+  """Halts the the specified Vagrant vm"""
+  local( 'vagrant/bin/vm.sh halt' + str(vm) )
 
