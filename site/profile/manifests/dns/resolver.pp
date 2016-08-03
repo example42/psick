48b14051c88b15c789c71e2b3e031adbb1f5091b
Alessandro Franceschi <al@example42.com>
2016-08-03 18:24:21 +0200

    Simple dns resolver profile

---
+++ site/profile/manifests/dns/resolver.pp
@@ -0,0 +1,21 @@
+# This class manages /etc/resolv.conf
+# Based on ghoneycutt-dnsclient
+class profile::dns::resolver (
+  Array $nameservers           = ['8.8.8.8','8.8.4.4'],
+  Array $options               = ['rotate','timeout:1'],
+  Optional[Array] $search      = undef,
+  Optional[String] $domain     = undef,
+  Optional[Array] $sortlist    = undef,
+  String $resolver_path        = '/etc/resolv.conf',
+  String $resolver_template    = 'profile/dns/resolver/resolv.conf.erb',
+) {
+
+  file { $resolver_path:
+    ensure  => present,
+    content => template($resolver_template),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+  }
+
+}
