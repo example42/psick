66b10a1ff5679fd4e6c552fb5dd32ee073b9e12e
Alessandro Franceschi <al@example42.com>
2016-08-23 14:50:45 +0200

    Simplified checks on base profiles

--- site/profile/manifests/base/linux.pp
+++ site/profile/manifests/base/linux.pp
@@ -22,71 +22,71 @@ class profile::base::linux (
 
 ) {
 
-  if $pre_class and $pre_class != '' {
+  if $pre_class != '' {
     contain $pre_class
   }
 
-  if $network_class and $network_class != '' {
+  if $network_class != '' {
     contain $network_class
     Class[$pre_class] -> Class[$network_class]
   }
 
-  if $mail_class and $mail_class != '' {
+  if $mail_class != '' {
     contain $mail_class
     Class[$pre_class] -> Class[$mail_class]
   }
 
-  if $puppet_class and $puppet_class != '' {
+  if $puppet_class != '' {
     contain $puppet_class
     Class[$pre_class] -> Class[$puppet_class]
   }
 
-  if $ssh_class and $ssh_class != '' {
+  if $ssh_class != '' {
     contain $ssh_class
     Class[$pre_class] -> Class[$ssh_class]
   }
 
-  if $monitor_class and $monitor_class != '' {
+  if $monitor_class != '' {
     contain $monitor_class
     Class[$pre_class] -> Class[$monitor_class]
   }
 
-  if $backup_class and $backup_class != '' {
+  if $backup_class != '' {
     contain $backup_class
     Class[$pre_class] -> Class[$backup_class]
   }
 
-  if $users_class and $users_class != '' {
+  if $users_class != '' {
     contain $users_class
     Class[$pre_class] -> Class[$users_class]
   }
 
-  if $sudo_class and $sudo_class != '' {
+  if $sudo_class != '' {
     contain $sudo_class
     Class[$pre_class] -> Class[$sudo_class]
   }
 
-  if $firewall_class and $firewall_class != '' {
+  if $firewall_class != '' {
     contain $firewall_class
     Class[$pre_class] -> Class[$firewall_class]
   }
 
-  if $logs_class and $logs_class != '' {
+  if $logs_class != '' {
     contain $logs_class
     Class[$pre_class] -> Class[$logs_class]
   }
 
-  if $time_class and $time_class != '' {
+  if $time_class != '' {
     contain $time_class
     Class[$pre_class] -> Class[$time_class]
   }
 
-  if $sysctl_class and $sysctl_class != '' {
+  if $sysctl_class != '' {
     contain $sysctl_class
     Class[$pre_class] -> Class[$sysctl_class]
   }
 
-  if $dns_class and $dns_class != '' {
+  if $dns_class != '' {
     contain $dns_class
     Class[$pre_class] -> Class[$dns_class]
   }
--- site/profile/manifests/base/windows.pp
+++ site/profile/manifests/base/windows.pp
@@ -17,46 +17,46 @@ class profile::base::windows (
 
 ) {
 
-  if $pre_class and $pre_class != '' {
+  if $pre_class != '' {
     contain $pre_class
   }
 
-  if $network_class and $network_class != '' {
+  if $network_class != '' {
     contain $network_class
     Class[$pre_class] -> Class[$network_class]
   }
 
-  if $mail_class and $mail_class != '' {
+  if $mail_class != '' {
     contain $mail_class
     Class[$pre_class] -> Class[$mail_class]
   }
 
-  if $puppet_class and $puppet_class != '' {
+  if $puppet_class != '' {
     contain $puppet_class
     Class[$pre_class] -> Class[$puppet_class]
   }
 
-  if $monitor_class and $monitor_class != '' {
+  if $monitor_class != '' {
     contain $monitor_class
     Class[$pre_class] -> Class[$monitor_class]
   }
 
-  if $backup_class and $backup_class != '' {
+  if $backup_class != '' {
     contain $backup_class
     Class[$pre_class] -> Class[$backup_class]
   }
 
-  if $users_class and $users_class != '' {
+  if $users_class != '' {
     contain $users_class
     Class[$pre_class] -> Class[$users_class]
   }
 
-  if $firewall_class and $firewall_class != '' {
+  if $firewall_class != '' {
     contain $firewall_class
     Class[$pre_class] -> Class[$firewall_class]
   }
 
-  if $logs_class and $logs_class != '' {
+  if $logs_class != '' {
     contain $logs_class
     Class[$pre_class] -> Class[$logs_class]
   }
