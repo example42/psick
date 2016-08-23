0b8d4bbd35de2f49492a11e54ed3d91c028b2aa4
Alessandro Franceschi <al@example42.com>
2016-08-23 12:40:46 +0200

    Basic static users management

--- site/profile/manifests/users/static.pp
+++ site/profile/manifests/users/static.pp
@@ -1,5 +1,19 @@
-class profile::users::static {
+class profile::users::static (
+  Hash $users         = {},
+  Hash $managed_users = {},
+  Boolean $delete_unmanaged = false,
+) {
 
-  # TODO
-
+  if $users != {} {
+    create_resources('user',$users)
+  }
+  if $managed_users != {} {
+    create_resources('profile::users::managed',$managed_users)
+  }
+  if $delete_unmanaged {
+    resources { 'user':
+     purge  => true,
+     unless_system_user => true,
+    }
+  }
 }
