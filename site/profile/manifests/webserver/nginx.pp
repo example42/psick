43225a0b9026365289046daf38323525b239efdb
Alessandro Franceschi <al@example42.com>
2016-10-10 20:21:43 +0200

    Experimental tp::install function in nginx profile

--- site/profile/manifests/webserver/nginx.pp
+++ site/profile/manifests/webserver/nginx.pp
@@ -1,13 +1,18 @@
 #
-class profile::webserver::nginx {
+class profile::webserver::nginx (
+  Enum['present','absent'] $ensure                     = 'present',
 
-  ::tp::install { 'nginx' : }
+  Variant[String[1],Undef] $config_dir_source          = undef,
+  String                   $config_file_template       = 'profile/webserver/nginx/nginx.conf.erb',
+) {
+
+  tp::install('nginx', { ensure => $ensure })
   $nginx_options = {
     'worker_processes'   => '12',
     'worker_connections' => '512',
   }
   tp::conf { 'nginx':
-    template     => 'profile/webserver/nginx/nginx.conf.erb',
+    template     => $config_file_template,
     options_hash => $nginx_options,
   }
 
