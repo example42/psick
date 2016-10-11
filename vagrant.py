5e6a81fe97303cadcc2782b96a5cebc002b57214
Christian Meißner <c.meissner@bnotk.de>
2016-10-11 06:59:51 +0200

    vagrant.py 	* added ability to get status of a given environement

--- vagrant.py
+++ vagrant.py
@@ -5,7 +5,10 @@ main_dir = subprocess.check_output("git rev-parse --show-toplevel", shell=True).
 @task
 def all_status(env=''):
   """Run vagrant status on all the available environments"""
-  local( 'cd ' + main_dir + '/vagrant/environments ; for v in $(ls ' + str(env) + '); do cd $v ; echo "Vagrant environment: ${v}" ; echo ; vagrant status ; cd ../ ; echo ; done' )
+  if env:
+    local( 'cd ' + main_dir + '/vagrant/environments/' + env + ' echo "Vagrant environment: ${v}" ; echo ; vagrant status ; cd - ; echo ')
+  else:
+    local( 'cd ' + main_dir + '/vagrant/environments ; for v in $(ls ' + str(env) + '); do cd $v ; echo "Vagrant environment: ${v}" ; echo ; vagrant status ; cd ../ ; echo ; done' )
 
 @task
 def status(vm=''):
@@ -33,7 +36,7 @@ def reload(vm=''):
   local( main_dir + '/vagrant/bin/vm.sh reload ' + str(vm) )
 
 @task
-def provision(vm=''):
+def provision(env='', vm=''):
   """Provision all or the specified vm"""
   local( main_dir + '/vagrant/bin/vm.sh provision ' + str(vm) )
 
