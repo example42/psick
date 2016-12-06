56a17e0f9f97e85b577da7ce89404a1fb1ea8ec5
Alessandro Franceschi <al@example42.com>
2016-12-06 19:14:03 +0100

    Added sample gitlab profile

---
+++ site/profile/manifests/gitlab.pp
@@ -0,0 +1,36 @@
+#
+class profile::gitlab (
+  String                $ensure      = 'present',
+
+  Variant[Undef,String] $template    = undef,
+  Hash                  $options     = { },
+  
+  Boolean $manage_installation       = true,
+
+) {
+
+  if $manage_installation {
+    $options_default = {
+    }
+    $gitlab_options = $options + $options_default
+    ::tp::install { 'gitlab-ce' :
+      ensure => $ensure,
+    }
+
+    if $template {
+      ::tp::conf { 'gitlab-ce':
+        ensure       => $ensure,
+        template     => $template,
+        options_hash => $gitlab_options,
+        notify       => Exec['gitlab-ctl reconfigure'],
+      }
+    }
+
+    exec { 'gitlab-ctl reconfigure':
+      refreshonly => true,
+      timeout     => '600',
+      subscribe   => Package['gitlab-ce'],
+    }
+  }
+
+}
