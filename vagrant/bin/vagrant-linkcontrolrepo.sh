86ec84ba6291d5a5d75d3c648af156db52c9d639
Martin Alfke <ma@example42.com>
2017-02-12 14:46:08 +0100

    GITLab in PE & linking control repo
    - make git.lan (from PE vagrant environment) same as git.demo
    - move only existing directories when linking control repo

--- vagrant/bin/vagrant-linkcontrolrepo.sh
+++ vagrant/bin/vagrant-linkcontrolrepo.sh
@@ -4,7 +4,9 @@ mount_dir=${2:-'/vagrant_puppet'}
 
 link_controlrepo() {
   echo "### Linking Puppet ${1} environment to local development directory."
-  mv /etc/puppetlabs/code/environments/$1 /etc/puppetlabs/code/environments/$1_$(date +%Y%m%d_%H%M%S)
+  if [ -d /etc/puppetlabs/code/environments/$1 ]; then
+    mv /etc/puppetlabs/code/environments/$1 /etc/puppetlabs/code/environments/$1_$(date +%Y%m%d_%H%M%S)
+  fi
   ln -sf $mount_dir /etc/puppetlabs/code/environments/$1
 }
 
