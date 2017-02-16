798803bdd8b3cd0f3e01a6fc7b724a2fbd6c6692
alvagante <al@example42.com>
2017-02-16 22:15:43 +0100

    Major vagrant refactoring WIP

--- vagrant/bin/vagrant-linkcontrolrepo.sh
+++ vagrant/bin/vagrant-linkcontrolrepo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-env=${1:production}
-mount_dir=${2:'/vagrant_puppet'}
+env=${1:-production}
+mount_dir=${2:-'/vagrant_puppet'}
 
 link_controlrepo() {
-  echo "### Linking Puppet ${1} environment to local development directory."
-  mv /etc/puppetlabs/code/environments/$1 /etc/puppetlabs/code/environments/$1_$(date +%Y%m%d_%H%M%S)
-  ln -sf $mount_dir /etc/puppetlabs/code/environments/$1
+  echo "### Linking Puppet ${env} environment to local development directory."
+  [ -d /etc/puppetlabs/code/environments/$env ] && mv /etc/puppetlabs/code/environments/$env /etc/puppetlabs/code/environments/$env_$(date +%Y%m%d_%H%M%S)
+  ln -sf $mount_dir /etc/puppetlabs/code/environments/$env
 }
 
-[ -L /etc/puppetlabs/code/environments/production ] || link_controlrepo $env
+[ -L /etc/puppetlabs/code/environments/$env ] || link_controlrepo $env
 
