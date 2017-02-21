24fb76e755ad5cf10cce3dcdf18d748748bf5b87
Alessandro Franceschi <al@example42.com>
2017-02-21 14:34:56 +0100

    CI improv + manageiq vm in lab

--- .octocatalog-diff.cfg.rb
+++
@@ -1,9 +0,0 @@
-settings[:hiera_config] = 'hiera.yaml'
-
-settings[:enc] = 'bin/enc_cat.sh'
-
-# settings[:puppet_binary] = '/opt/puppetlabs/puppet/bin/puppet'
-
-settings[:puppetdb_url] = 'https://puppetdb:8081'
-#settings[:puppetdb_ssl_ca] = '/etc/puppetlabs/puppetdb/ssl/ca.pem'
-#settings[:puppetdb_ssl_client_cert] = File.read('/etc/puppetlabs/puppet/ssl/..')
