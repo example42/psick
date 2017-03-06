7219ce13ea6e4ef523b79dc577d4077f95cfd049
Stephen Nesbitt <snesbitt@samdom.aussieswithtails.fuzzbutt>
2017-03-06 11:09:05 -0800

    Checkpoint: Add profile::ssh::openssh unit tests covering cases where :ensure parameter is set to present or is set to an invalid value

--- site/profile/spec/classes/openssh_spec.rb
+++ site/profile/spec/classes/openssh_spec.rb
@@ -11,7 +11,7 @@ describe 'profile::ssh::openssh', :type => :class do
       let (:facts) do
         facts.merge(super())
       end
-      it { pp catalogue.resources }
+      # it { pp catalogue.resources }
       it { is_expected.to compile.with_all_deps }
       it { is_expected.to contain_class('profile::ssh::openssh') }
 
@@ -26,7 +26,7 @@ describe 'profile::ssh::openssh', :type => :class do
         ) }
       end
 
-      context 'ensure parameter specified' do
+      context ':ensure parameter specified' do
         let :default_params do
           {
               :config_dir_source => 'test_config_dir_source',
@@ -34,7 +34,7 @@ describe 'profile::ssh::openssh', :type => :class do
           }
         end
 
-        context 'with ensure parameter set to absent' do
+        describe 'with parameter: ensure parameter set to absent' do
           let :params do
             default_params.merge({:ensure => 'absent'})
           end
@@ -42,10 +42,12 @@ describe 'profile::ssh::openssh', :type => :class do
           it { is_expected.to contain_tp__install('openssh').with(
               'ensure' => 'absent'
           ) }
+
           it { is_expected.to contain_tp__dir('openssh').with(
               'ensure' => 'absent',
               'source' => 'test_config_dir_source'
           ) }
+
           it { is_expected.to contain_tp__conf('openssh').with(
               'ensure'       => 'absent',
               'template'     => '/dev/null',
@@ -53,6 +55,38 @@ describe 'profile::ssh::openssh', :type => :class do
                                   'option2' => 'option2_value'}
           ) }
         end
+
+        describe 'with parameter :ensure set to present' do
+          let :params do
+            default_params.merge({:ensure => 'present'})
+          end
+
+          it { is_expected.to contain_tp__install('openssh').with(
+              'ensure' => 'present'
+          ) }
+
+          it { is_expected.to contain_tp__conf('openssh').with(
+              'ensure'       => 'present',
+              'template'     => '/dev/null',
+              'options_hash' => { 'option1' => 'option1_value',
+                                  'option2' => 'option2_value'}
+          ) }
+
+          it { is_expected.to contain_tp__dir('openssh').with(
+              'ensure' => 'present',
+              'source' => 'test_config_dir_source'
+          ) }
+        end
+
+        describe 'with invalid :ensure not equal to "present" or "absent"' do
+          let :params do
+            {
+                :ensure => 'invalid value'
+            }
+          end
+          it {is_expected.to raise_error(Puppet::PreformattedError, /^Evaluation Error.* got \'invalid value\'.*/)}
+        end
+
       end
     end
   end
