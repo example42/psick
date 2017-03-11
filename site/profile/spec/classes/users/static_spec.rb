e47688dec7096dd5eed1d919ac473d1c62ad772c
Stephen Nesbitt <snesbitt@samdom.aussieswithtails.fuzzbutt>
2017-03-11 09:27:59 -0800

    Checkpoint: add users and managed_users has tests

--- site/profile/spec/classes/users/static_spec.rb
+++ site/profile/spec/classes/users/static_spec.rb
@@ -18,35 +18,66 @@ describe 'profile::users::static', :type => :class do
       it { is_expected.to contain_class('profile::users::static') }
 
       context 'with defaults values' do
-        it {is_expected.not_to contain_user('root')}
-        it {is_expected.to have_user_resource_count(0)}
-        it {is_expected.to have_tools__user__managed_resource_count(0)}
+        describe "Root user is not created"
+        it { is_expected.not_to contain_user('root') }
+
+        describe 'No users are created'
+        it { is_expected.to have_user_resource_count(0) }
+
+        describe 'Unmanaged users are not purged'
+        it { is_expected.to have_tools__user__managed_resource_count(0) }
       end
 
       context 'with rootpw: set' do
-        let :params do
-          {
-              :rootpw => 'test_rootpw'
-          }
-        end
-        it {is_expected.to contain_user('root').with(
+        let(:params) { {:rootpw => 'test_rootpw'} }
+
+        describe 'Root user is created'
+        it { is_expected.to contain_user('root').with(
             'password' => 'test_rootpw'
         ) }
       end
 
       context 'with :delete_unmanaged set to true' do
-        it { pp catalogue.resources }  # Uncomment to dump the catalogue
-        let :params do
-          {
-              :delete_unmanaged => true
-          }
-        end
-        it {is_expected.to contain_resources('user').with(
-            'purge'              => true,
+        # it { pp catalogue.resources }  # Uncomment to dump the catalogue
+        let(:params) { {:delete_unmanaged => true} }
+
+        describe "Non-system users are purged"
+        it { is_expected.to contain_resources('user').with(
+            'purge' => true,
             'unless_system_user' => true
-        )}
+        ) }
       end
 
+      context 'with :users defined' do
+        let(:params) { {
+            :users => {
+                'test_user1' => {
+                    "name" => 'user1'
+                },
+                'test_user2' => {
+                    "name" => 'user2'
+                }
+        }}
+        }
+        describe
+        it { is_expected.to have_user_resource_count(2) }
+      end
+
+      context 'with :managed_users defined' do
+        it { pp catalogue.resources } # Uncomment to dump the catalogue
+        let(:params) { {
+            :managed_users => {
+                'testmanaged_user1' => {
+                    "name" => 'managed_user1'
+                },
+                'test_user2' => {
+                    "name" => 'managed_user2'
+                }
+            }}
+        }
+        describe
+        it { is_expected.to have_tools__user__managed_resource_count(2)  }
+      end
     end
   end
 end
