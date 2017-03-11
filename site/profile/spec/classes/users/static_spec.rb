bdaad3e882d0150bab9841fea17888506f5c738f
Stephen Nesbitt <snesbitt@samdom.aussieswithtails.fuzzbutt>
2017-03-11 09:48:28 -0800

    Checkpoint: add invalid parameter values tests Cosmetic cleanup of spec_helper.rb

--- site/profile/spec/classes/users/static_spec.rb
+++ site/profile/spec/classes/users/static_spec.rb
@@ -4,21 +4,21 @@ facts_yaml = File.dirname(__FILE__) + '/../../fixtures/facts/spec.yaml'
 facts = YAML.load_file(facts_yaml)
 
 describe 'profile::users::static', :type => :class do
-  let(:facts) { facts }
+  let(:facts) do
+    facts
+  end
 
   on_supported_os.each do |os, facts|
     context "on #{os}" do
-      let (:facts) do
+      let((:facts)) do
         facts.merge(super())
       end
-
-      # it { pp catalogue.resources }  # Uncomment to dump the catalogue
 
       it { is_expected.to compile.with_all_deps }
       it { is_expected.to contain_class('profile::users::static') }
 
       context 'with defaults values' do
-        describe "Root user is not created"
+        describe 'Root user is not created'
         it { is_expected.not_to contain_user('root') }
 
         describe 'No users are created'
@@ -29,7 +29,9 @@ describe 'profile::users::static', :type => :class do
       end
 
       context 'with rootpw: set' do
-        let(:params) { {:rootpw => 'test_rootpw'} }
+        let(:params) do
+          {:rootpw => 'test_rootpw'}
+        end
 
         describe 'Root user is created'
         it { is_expected.to contain_user('root').with(
@@ -39,9 +41,11 @@ describe 'profile::users::static', :type => :class do
 
       context 'with :delete_unmanaged set to true' do
         # it { pp catalogue.resources }  # Uncomment to dump the catalogue
-        let(:params) { {:delete_unmanaged => true} }
+        let(:params) do
+          {:delete_unmanaged => true}
+        end
 
-        describe "Non-system users are purged"
+        describe 'Non-system users are purged'
         it { is_expected.to contain_resources('user').with(
             'purge' => true,
             'unless_system_user' => true
@@ -52,10 +56,10 @@ describe 'profile::users::static', :type => :class do
         let(:params) { {
             :users => {
                 'test_user1' => {
-                    "name" => 'user1'
+                    'name' => 'user1'
                 },
                 'test_user2' => {
-                    "name" => 'user2'
+                    'name' => 'user2'
                 }
         }}
         }
@@ -63,21 +67,29 @@ describe 'profile::users::static', :type => :class do
         it { is_expected.to have_user_resource_count(2) }
       end
 
-      context 'with :managed_users defined' do
-        it { pp catalogue.resources } # Uncomment to dump the catalogue
+      context('with :managed_users defined') {
         let(:params) { {
-            :managed_users => {
+            managed_users: {
                 'testmanaged_user1' => {
-                    "name" => 'managed_user1'
+                    'name' => 'managed_user1'
                 },
                 'test_user2' => {
-                    "name" => 'managed_user2'
+                    'name' => 'managed_user2'
                 }
             }}
         }
         describe
-        it { is_expected.to have_tools__user__managed_resource_count(2)  }
+        it { is_expected.to have_tools__user__managed_resource_count(2) }
+      }
+
+      context 'with invalid parameter values' do
+        describe ':rootpw cannot be empty string'
+          let(:params) do
+            {:rootpw => ''}
+          end
+          it { is_expected.to raise_error(Puppet::PreformattedError, /^Evaluation Error:.*/) }
       end
+
     end
   end
 end
