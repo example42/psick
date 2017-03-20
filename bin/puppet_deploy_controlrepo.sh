6a612314e04eaca1175169fc9577ce0fc40e98a6
Alessandro Franceschi <al@example42.com>
2017-03-20 20:45:16 +0100

    More robust deploy script for controlrepo

--- bin/puppet_deploy_controlrepo.sh
+++ bin/puppet_deploy_controlrepo.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+script_dir="$(dirname $0)"
+. "${script_dir}/functions"
+backup_dir="/etc/puppetlabs/code_backup"
+backup_date=$(date +'%Y%m%d-%H%M%S')
 environment=${1:-production}
+PATH=$PATH:/usr/local/bin
+
+echo_title "Installing prerequisite packages and gems"
+
 puppet resource package git ensure=present
 
 if [ $(facter osfamily) == 'Debian' ]; then
@@ -13,8 +21,22 @@ puppet resource package hiera-eyaml ensure=present provider=gem
 puppet resource package r10k ensure=present provider=gem
 
 mkdir -p /etc/puppetlabs/code/environments/
+
+if [ -d /etc/puppetlabs/code/environments/$environment ]; then
+  ask_interactive "Directory /etc/puppetlabs/code/environments/$environment exists. We have to move it."
+  if [ "$?" = 0 ]; then
+    mkdir -p $backup_dir
+    mv /etc/puppetlabs/code/environments/$environment "${backup_dir}/${environment}-${backup_date}"
+    echo_subtitle "/etc/puppetlabs/code/environments/$environment moved to ${backup_dir}/${environment}-${backup_date}"
+  else
+    echo "Can't proceed. Remove /etc/puppetlabs/code/environments/$environment or pass as argument a different environment"
+    exit 1
+  fi
+fi
+echo title "Cloning git://github.com/example42/psick.git to /etc/puppetlabs/code/environments/$environment"
 git clone git://github.com/example42/psick.git /etc/puppetlabs/code/environments/$environment
 cd /etc/puppetlabs/code/environments/$environment
+echo_title "Running r10k puppetfile install -v"
 r10k puppetfile install -v
-
+echo_title "Linking /etc/puppetlabs/puppet/hiera.yaml to /etc/puppetlabs/code/environments/$environment/hiera3.yaml"
 ln -sf /etc/puppetlabs/code/environments/$environment/hiera3.yaml /etc/puppetlabs/puppet/hiera.yaml
--- fabfile/puppet.py
+++ fabfile/puppet.py
@@ -75,10 +75,11 @@ def current_config():
   sudo( 'cat $(puppet config print lastrunfile) | grep "config: " | cut -d ":" -f 2- ' )
 
 @task
-def deploy_controlrepo():
+def deploy_controlrepo(puppet_env='production'):
   """[remote] Deploy this control repo on a node (Puppet has to be already installed)"""
+  put( "bin/functions","/var/tmp/functions",mode=755 )
   put( "bin/puppet_deploy_controlrepo.sh","/var/tmp/puppet_deploy_controlrepo.sh",mode=755 )
-  sudo ( "/var/tmp/puppet_deploy_controlrepo.sh" )
+  sudo ( "/var/tmp/puppet_deploy_controlrepo.sh " + str(puppet_env) )
 
 @task
 def install(os=''):
