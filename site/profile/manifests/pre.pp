d8eed8e0715238ecace32afba4fc5ec4c88f289c
Alessandro Franceschi <al@example42.com>
2017-03-23 10:22:27 +0100

    Hiera5 improvements in profile

--- site/profile/manifests/pre.pp
+++ site/profile/manifests/pre.pp
@@ -3,38 +3,29 @@
 # use.
 # This profile class is the only one included by default on the base profiles.
 #
-# @param include_defaults Define if to include the default prerequisites class
-#                         which manages subscriptions to Satellite or similar
-#                         central repository management systems
 # @param rpmgpg_class Name of the class to include to manage gpgkeys on rpm
 #                     based systems
+# @param rhn_class Name of the class to include to manage RHN on RedHat family
 # @param repo_class Name of the class to include to manage additional repos
-# @param repo_class Name of the class to include to the system's proxy server
+# @param proxy_class Name of the class to include to the system's proxy server
 #
 # @example Including additional classes for rpmgpg and repo
 #    profile::pre::rpmgpg_class: '::profile::pre::gpgkeys'
 #    profile::pre::repo_class: '::profile::repo'
 #
 class profile::pre (
-  Boolean $include_defaults,
   String $rpmgpg_class,
+  String $rhn_class,
   String $repo_class,
   String $proxy_class,
 ) {
 
-  if $include_defaults {
-    case $::operatingsystem {
-      'RedHat': { contain ::profile::pre::rhn }
-      'OracleLinux': { contain ::profile::pre::rhn }
-      'CentOS': { contain ::profile::pre::rhn }
-      'SLES': { contain ::profile::pre::rhn }
-      'Ubuntu': { contain ::profile::pre::ubuntu }
-      default: { notice("No pre class for ${::operatingsystem}") }
-    }
-  }
-
   if $rpmgpg_class != '' and $::osfamily =~ /RedHat|Suse/ {
     contain $rpmgpg_class
+  }
+
+  if $rhn_class != '' and $::osfamily =~ /RedHat|Suse/ {
+    contain $rhn_class
   }
 
   if $repo_class != '' {
