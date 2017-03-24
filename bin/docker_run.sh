c33ddf15416acf4b60019e714d396ae2c7be42bd
Alessandro Franceschi <al@example42.com>
2017-03-24 12:01:09 +0100

    A few fixes on CI

--- bin/docker_run.sh
+++ bin/docker_run.sh
@@ -3,9 +3,13 @@ repo_dir=$(git rev-parse --show-toplevel)
 . "${repo_dir}/bin/functions"
 . "${repo_dir}/bin/config/defaults"
 
-registry="myregistry.domain.name/puppet/"
-image="centos-7"
-version="latest"
+# Customise here the default registry (can be
+# defined via the --registry argument). Default
+# hub.docker.com is used with example42 puppet-agent
+# registry="docker.example.com:5000/puppet"
+registry="example42"
+image="puppet-agent"
+version="centos-7"
 
 env=$docker_env
 datacenter=$docker_dc
@@ -27,6 +31,9 @@ while [ $# -gt 0 ]; do
   case "$CMD" in 
     --tags)
       runcommand="${runcommand} --tags ${PARAM}"
+    ;;
+    --registry)
+      registry=${PARAM:-$registry}
     ;;
     --image)
       image=${PARAM:-$image}
@@ -65,11 +72,11 @@ done
 
 params="-e FACTER_pp_environment=${env} -e FACTER_pp_role=${role} -e FACTER_pp_datacenter=${datacenter} -e FACTER_pp_application=${application} -e FACTER_pp_zone=${zone} $custom"
 name="${image}_${version}-${role}-$(date +'%H%M%S')" 
-container="${registry}${image}:${version}" 
+container="${registry}/${image}:${version}" 
 PATH=$PATH:/opt/puppetlabs/bin
 echo_title "## Running $runcommand on image ${image}:${version} from ${repo_dir}"
 echo_subtitle "Defined facts: $params"
-docker pull "${registry}${image}:${version}"
+docker pull "${registry}/${image}:${version}"
 docker run $options -v "${repo_dir}:/etc/puppetlabs/code/environments/production"  --name="${name}" $params "${container}" $runcommand
 exitstatus=$? 
 docker rm -f "${name}"
