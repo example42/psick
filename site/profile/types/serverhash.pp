c428e512a67e4bf6cc21412bb88b6fd02a2ce282
Alessandro Franceschi <al@example42.com>
2017-06-15 18:12:00 +0200

    More tolerant port param type check for serverhash type

--- site/profile/types/serverhash.pp
+++ site/profile/types/serverhash.pp
@@ -2,7 +2,7 @@ type Profile::Serverhash = Struct[{
 
   Optional[scheme] => String,
   Optional[host] => String,
-  Optional[port] => String,
+  Optional[port] => Variant[Integer, String],
   Optional[user] => String,
   Optional[password] => String,
   Optional[path] => String,
