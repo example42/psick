3c78ed14dc85759b21f94315615a95899160bd5f
Alessandro Franceschi <al@example42.com>
2017-06-15 18:31:16 +0200

    Auto class list refinements, the re ignorant way

--- bin/docs_classlistgenerate.sh
+++ bin/docs_classlistgenerate.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 repo_dir=$(git rev-parse --show-toplevel)
 . "${repo_dir}/bin/functions"
+
 target_file=${1:-'docs/classes.md'}
 [ -f $target_file ] && rm -f $target_file
 
@@ -10,7 +11,7 @@ echo >> $target_file
 echo "This is an automatically generated list of the profile classes in this control-repo" >> $target_file
 echo >> $target_file
 for f in $(find site/profile/manifests/ -name *.pp); do
-  a=$(grep ^class ${f} | sed 's/(//g' | sed 's/ $//g' | sed 's/^class //g')
+        a=$(grep ^class ${f} | sed 's/(//g' | sed 's/{//g' | sed 's/)//g' | sed 's/ *$//g' | sed 's/^class //g')
   b=$(grep @summary ${f} | sed 's/^# *@summary/ -/g') 
   echo "  - **${a}**${b}" >> $target_file
 done
