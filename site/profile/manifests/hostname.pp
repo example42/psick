bc01a897e8b4b41125f5f1f4895aef1034cafba5
Alessandro Franceschi <al@example42.com>
2017-09-10 09:35:16 +0200

    Firstrun refinement and profile::hostname support for Windows

--- site/profile/manifests/hostname.pp
+++ site/profile/manifests/hostname.pp
@@ -17,49 +17,67 @@ class profile::hostname (
     $calc_fqdn = $host
   }
 
-  if $::virtual != 'docker' {
-    file { '/etc/hostname':
-      ensure  => file,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => "${calc_fqdn}\n",
-      notify  => Exec['apply_hostname'],
-    }
-
-    exec { 'apply_hostname':
-      command => '/bin/hostname -F /etc/hostname',
-      unless  => '/usr/bin/test `hostname` = `/bin/cat /etc/hostname`',
-    }
-
-    if $update_host_entry {
-      host { $host:
-        ensure       => present,
-        host_aliases => $calc_fqdn,
-        ip           => $ip,
-      }
-    }
-
-    if $update_network_entry {
-      case $::osfamily {
-        'RedHat': {
-          file { '/etc/sysconfig/network':
+  case $::kernel {
+    'Linux': {
+      if $::virtual != 'docker' {
+        file { '/etc/hostname':
+          ensure  => file,
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+          content => "${calc_fqdn}\n",
+          notify  => Exec['apply_hostname'],
+        }
+    
+        exec { 'apply_hostname':
+          command => '/bin/hostname -F /etc/hostname',
+          unless  => '/usr/bin/test `hostname` = `/bin/cat /etc/hostname`',
+        }
+    
+        if $update_host_entry {
+          host { $host:
+            ensure       => present,
+            host_aliases => $calc_fqdn,
+            ip           => $ip,
+          }
+        }
+    
+        if $update_network_entry {
+          case $::osfamily {
+            'RedHat': {
+              file { '/etc/sysconfig/network':
+                ensure  => file,
+                content => "NETWORKING=yes\nNETWORKING_IPV6=no\nHOSTNAME=${calc_fqdn}\n",
+                notify  => Exec['apply_hostname'],
+              }
+            }
+            default: {}
+          }
+        }
+    
+        if $update_cloud_cfg {
+          file { '/etc/cloud/cloud.cfg.d/99_preserve_hostname.cfg':
             ensure  => file,
-            content => "NETWORKING=yes\nNETWORKING_IPV6=no\nHOSTNAME=${calc_fqdn}\n",
+            content => "preserve_hostname: true\n",
             notify  => Exec['apply_hostname'],
           }
         }
-        default: {}
       }
     }
-
-    if $update_cloud_cfg {
-      file { '/etc/cloud/cloud.cfg.d/99_preserve_hostname.cfg':
-        ensure  => file,
-        content => "preserve_hostname: true\n",
-        notify  => Exec['apply_hostname'],
+    'windows': {
+      registry_value { 'HKLM\System\CurrentControlSet\Control\ComputerName\ActiveComputerName':
+        ensure => 'present',
+        type   => string,
+        data   => $host,
+      }
+      registry_value { 'HKLM\System\CurrentControlSet\Control\ComputerName\ComputerName':
+        ensure => 'present',
+        type   => string,
+        data   => $host,
       }
     }
-
+    default: {
+      notice("profile::hostname does not support $::kernel")
+    }
   }
 }
