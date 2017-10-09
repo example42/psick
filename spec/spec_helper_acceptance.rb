eb40a9f334c8d89654ad0631bd969eac07b305ec
Martin Alfke <ma@example42.com>
2017-10-09 11:51:18 -0700

    Make beaker vagrant working
    - mount proper path
    - prepare system

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -14,6 +14,10 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     hosts.each do |host|
+      on(host, '/opt/puppetlabs/bin/puppet resource package git ensure=present')
+      on(host, '/opt/puppetlabs/bin/puppet resource file /etc/puppetlabs/code/environments/production/modules ensure=absent recurse=true force=true')
+      on(host, '/opt/puppetlabs/puppet/bin/gem install r10k')
+      on(host, 'cd /etc/puppetlabs/code/environments/production && /opt/puppetlabs/puppet/bin/r10k puppetfile install -v') 
       on(host, '/opt/puppetlabs/puppet/bin/gem install hiera-eyaml')
       on(host, 'cd /etc/puppetlabs/puppet/ && /opt/puppetlabs/puppet/bin/eyaml createkeys')
     end
