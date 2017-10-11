f4a5c569fe8eb91e4cdae1204df859219bfaf3f1
Alessandro Franceschi <al@example42.com>
2017-10-11 19:19:18 +0200

    ..

--- bin/puppet_check_rake.sh
+++ bin/puppet_check_rake.sh
@@ -29,6 +29,17 @@ if [ ! -z ${RAKE} ] && [ ! -z ${RUBY} ] ; then
       global_exit=1
     fi
   done
+
+  echo_title "Running rspec tests on psick module"
+  cd "${repo_dir}/modules/psick"
+  rake spec
+  if [ $? == 0 ]; then
+    echo_success "OK"
+  else
+    echo_failure "ERROR"
+    echo $err
+    global_exit=1
+  fi
 else
   echo_warning "rake not found."
 fi
