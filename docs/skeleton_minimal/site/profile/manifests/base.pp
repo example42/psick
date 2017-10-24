ab4db37f25fcf71b79a9bdc97e16f96c95e1bb6d
Alessandro Franceschi <al@example42.com>
2017-10-24 09:05:49 +0200

    Resume skeleton_minimal and fix #201

---
+++ docs/skeleton_minimal/site/profile/manifests/base.pp
@@ -0,0 +1,10 @@
+# Sample profile::base
+# It should include resources common to all nodes
+#
+class profile::base {
+
+  # Common resources here.
+  # See https://github.com/example42/psick/blob/production/site/profile/manifests/base/linux.pp
+  # for an example
+
+}
