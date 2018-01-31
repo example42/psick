5f229202af4e42cf9bae446e42261a2f750981b3
Alessandro Franceschi <al@example42.com>
2018-01-31 19:09:39 +0100

    Use gitlab cli vars for Danger

--- bin/gitlab_danger.sh
+++ bin/gitlab_danger.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 PATH=/opt/puppetlabs/puppet/bin:$PATH
 echo "Run Danger"
-. /etc/gitlab-cli.conf
+cli_vars=$(cat /etc/gitlab-cli.conf)
+for v in $cli_vars; do
+  export $v
+done
 danger --verbose
