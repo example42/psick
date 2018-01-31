05eb3a927843e47058394b8fa1a4ed808fe41571
Alessandro Franceschi <al@example42.com>
2018-01-31 20:22:52 +0100

    Started change process docs reorg

--- bin/gitlab_danger.sh
+++ bin/gitlab_danger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=/opt/puppetlabs/puppet/bin:$PATH
 echo "Run Danger"
-cli_vars=$(cat /etc/gitlab-cli.conf)
+cli_vars=$(cat /etc/gitlab-cli.conf|grep -v "^#"|grep -v "^$")
 for v in $cli_vars; do
   export $v
 done
