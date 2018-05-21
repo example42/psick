9d051ac339d7138ebe55719a117f5fd909cc6138
Shachar Shapira <Shachar.shapira@automatit.com>
2018-05-21 11:48:12 +0000

    Fixing ssh config configuration

--- site/profile/manifests/ssh_windows.pp
+++ site/profile/manifests/ssh_windows.pp
@@ -1,15 +1,9 @@
 # @class jenkins
 #
 class profile::ssh_windows (
-
-
   String $home_dir                          = 'c:\\Users\\Administrator',
-
 ) {
-
   file { "${home_dir}/.ssh" :
       ensure  => directory,
   }
-
-
 }
