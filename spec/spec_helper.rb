786a132e76f8dba77f7a3f42e1b1b3b1fae51534
Alessandro Franceschi <al@example42.com>
2018-09-05 11:52:41 +0100

    Executed pdk convert

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,44 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
-include RspecPuppetFacts
 
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
-
-support_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec/support/*.rb'))
-Dir[support_path].each {|f| require f}
-
-RSpec.configure do |c|
-  c.module_path = File.join(fixture_path, 'modules/site') + ':' + File.join(fixture_path, 'modules/r10k')
-  c.manifest_dir = File.join(fixture_path, '../../manifests')
-  c.manifest = File.join(fixture_path, '../../manifests/site.pp')
-  c.hiera_config = File.join(fixture_path, '../../hiera.yaml')
-  c.fail_fast = true
+begin
+  require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
+rescue LoadError => loaderror
+  warn "Could not require spec_helper_local: #{loaderror.message}"
 end
 
-at_exit { RSpec::Puppet::Coverage.report! }
+include RspecPuppetFacts
+
+default_facts = {
+  puppetversion: Puppet.version,
+  facterversion: Facter.version,
+}
+
+default_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml'))
+default_module_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml'))
+
+if File.exist?(default_facts_path) && File.readable?(default_facts_path)
+  default_facts.merge!(YAML.safe_load(File.read(default_facts_path)))
+end
+
+if File.exist?(default_module_facts_path) && File.readable?(default_module_facts_path)
+  default_facts.merge!(YAML.safe_load(File.read(default_module_facts_path)))
+end
+
+RSpec.configure do |c|
+  c.default_facts = default_facts
+  c.before :each do
+    # set to strictest setting for testing
+    # by default Puppet runs at warning level
+    Puppet.settings[:strict] = :warning
+  end
+end
+
+def ensure_module_defined(module_name)
+  module_name.split('::').reduce(Object) do |last_module, next_module|
+    last_module.const_set(next_module, Module.new) unless last_module.const_defined?(next_module)
+    last_module.const_get(next_module)
+  end
+end
+
+# 'spec_overrides' from sync.yml will appear below this line
