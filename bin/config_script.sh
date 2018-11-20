19fcdb4e2d95b0121cd546dd5c756b67ac8a2e4e
Martin Alfke <ma@example42.com>
2018-11-20 08:59:21 +0100

    On PE use code-staging instead of code dir
    this allows output based on latest control-repo git commit instead of
    showing code manager post staging commit to be shown

--- bin/config_script.sh
+++ bin/config_script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+CODEDIR='/etc/puppetlabs/code'   # better: $(puppet master --configprint codedir)
+CODESTAGEDIR='/etc/puppetlabs/code-staging'  # better: "($puppet master --configprint codedir)-staging"
 if [ -x /usr/bin/git ]; then
-  /usr/bin/git --git-dir /etc/puppetlabs/code/environments/$1/.git log --pretty=format:"%h - %an, %ad : %s" -1
+  if [ -d $CODESTAGEDIR ]; then
+    ENVGITDIR="${CODESTAGEDIR}/environments/$1/.git"
+  else
+    ENVGITDIR="${CODEDIR}/environments/$1/.git"
+  fi
+  /usr/bin/git --git-dir $ENVGITDIR log --pretty=format:"%h - %an, %ad : %s" -1
 else
   echo "no git - environment $1"
 fi
+
