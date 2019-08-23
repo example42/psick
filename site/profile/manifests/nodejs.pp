80ffd0fad5fa6d5c7bf1828cd3181d06ce6c0682
Vitalii Lukianenko <vitalii.lukianenko@automat-it.com>
2019-08-23 12:42:37 +0300

    add ensure node installation

--- site/profile/manifests/nodejs.pp
+++ site/profile/manifests/nodejs.pp
@@ -5,6 +5,8 @@ class profile::nodejs (
 {
   class { 'nodejs':
     repo_url_suffix => $version,
+    nodejs_dev_package_ensure => 'present',
+    npm_package_ensure        => 'present',
   }
   $npm_packages.each |String $npm_package| {
     package { $npm_package:
