0b638c5e88606b5ecc4eff0b7f6c014090d3e579
Martin Alfke <ma@example42.com>
2020-05-28 16:36:01 +0200

    Summary from hcloud
    echo into scripts what is done

--- bin/bootstrap/puppet_foss.sh
+++ bin/bootstrap/puppet_foss.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-hostname=$(hostname)
-
-vagrant/bin/vagrant-sethostname.sh $hostname
-vagrant/bin/vagrant-sethosts.sh $hostname 127.0.0.1
+echo "Set external facts"
 bin/puppet_set_external_facts.sh --role='puppet' --env 'host' --zone 'foss' --datacenter 'vagrant' --application 'puppetinfra'
+echo "Set trusted facts"
 bin/puppet_set_trusted_facts.sh --role='puppet' --env 'host' --zone 'foss' --datacenter 'vagrant' --application 'puppetinfra'
+echo "Install puppet"
 bin/puppet_install.sh '5'
+echo "Deploy Puppet Master"
 vagrant/bin/vagrant-setup_puppetserver.sh 'host'
+echo "Deploy control-repository"
 bin/puppet_deploy_controlrepo.sh
+echo "Starting Puppet Server"
 /opt/puppetlabs/bin/puppet resource service puppetserver ensure=running enable=true
 
