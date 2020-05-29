ee9086c2ced1993c512034379037ea0524c41db8
Martin Alfke <ma@example42.com>
2020-05-29 10:35:49 +0200

    Student Machines
    - crerate IP outpout on creation
    - install puppet agent

---
+++ bin/bootstrap/student.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Set external facts"
+bin/puppet_set_external_facts.sh --role='student' --env 'host' --zone 'foss' --datacenter 'vagrant' --application 'puppetinfra'
+echo "Set trusted facts"
+bin/puppet_set_trusted_facts.sh --role='student' --env 'host' --zone 'foss' --datacenter 'vagrant' --application 'puppetinfra'
+echo "Installing Puppet"
+bin/puppet_install.sh '5'
+
