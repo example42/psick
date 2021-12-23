97c3b90cfeec0d20b5847c60e4e46f33b917aaf2
Alessandro Franceschi <al@example42.com>
2021-12-23 21:59:43 +0100

    Fabric cleanup

--- site/role/manifests/sample.pp
+++
@@ -1,9 +0,0 @@
-# Sample role class. This is not actually used.
-# Edit puppet-modules/manifests/site.pp to define profiles
-# in role classes instead of looking for them on hiera
-#
-class role::sample {
-
-  include ::profile::sample
-
-}
