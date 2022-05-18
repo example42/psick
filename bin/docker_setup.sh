0897824cb13cec6b01a816d3c6851ba749cb2021
Alessandro Franceschi <al@example42.com>
2022-05-18 13:16:02 +0200

    Adapted data for mysql mariadb gitlab docker apache profiles

--- bin/docker_setup.sh
+++ bin/docker_setup.sh
@@ -5,6 +5,6 @@ repo_dir=$(git rev-parse --show-toplevel)
 puppet_options="--modulepath ${repo_dir}/site:${repo_dir}/modules:/etc/puppet/modules --environmentpath ${repo_dir} --hiera_config ${repo_dir}/hiera.yaml"
 
 echo_title "Going to install Docker"
-echo_subtitle "You need sudo powers. Executing: sudo -E puppet apply -e 'include psick::docker'"
-sudo -E puppet apply $puppet_options -e 'include psick ; include psick::docker'
+echo_subtitle "You need sudo powers. Executing: sudo -E puppet apply -e 'include psick_profile::docker'"
+sudo -E puppet apply $puppet_options -e 'include psick ; include psick_profile::docker'
 
