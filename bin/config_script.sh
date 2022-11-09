169389f3ba3034a962e0b4573527aebc664c091c
Alessandro Franceschi <al@example42.com>
2022-11-09 17:50:21 +0100

    Updated config_script

--- bin/config_script.sh
+++ bin/config_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-CODEDIR='/etc/puppetlabs/code'   # better: $(puppet master --configprint codedir)
-CODESTAGEDIR='/etc/puppetlabs/code-staging'  # better: "($puppet master --configprint codedir)-staging"
+ENVDIR='/etc/puppetlabs/code/environments'
+ENVSTAGEDIR='/etc/puppetlabs/code-developers'  # Custom dir used for developers environments
 if [ -x /usr/bin/git ]; then
-  if [ -d $CODESTAGEDIR ]; then
-    ENVGITDIR="${CODESTAGEDIR}/environments/$1/.git"
+  if [ -d "${ENVSTAGEDIR}/$1/.git" ]; then
+    ENVGITDIR="${ENVSTAGEDIR}/$1/.git"
   else
-    ENVGITDIR="${CODEDIR}/environments/$1/.git"
+    ENVGITDIR="${ENVDIR}/$1/.git"
   fi
-  /usr/bin/git --git-dir $ENVGITDIR log --pretty=format:"%h - %an, %ad : %s" -1
+  /usr/bin/git --git-dir "${ENVGITDIR}" log --pretty=format:"%h - %an, %ad : %s" -1
 else
-  echo "no git - environment $1"
+  echo "no git - environment ${1}"
 fi
 exit 0
